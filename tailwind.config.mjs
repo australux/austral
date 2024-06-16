450ee93bd86f1a7dcdc01f857e1e0ef02741fa49
Luciano Montilla <unonueve95@gmail.com>
2024-06-16 17:34:26 -0300

    UI responsive refactoring

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {
             fontFamily: {
-                chivo: ["Chivo Mono", "monospace"],
+                chivo: ["Chivo Mono", ...defaultTheme.fontFamily.mono],
             },
         },
     },
-    plugins: [],
+    plugins: [require("tailwind-scrollbar")],
 };
